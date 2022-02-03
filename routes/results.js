20fb981c725880e38000fd5952ed843f9a434584
darshan260802 <91478282+darshan260802@users.noreply.github.com>
2022-02-03 13:49:19 +0530

    fun

--- routes/results.js
+++ routes/results.js
@@ -8,11 +8,15 @@ const router = express.Router();
 
 router.get("/results", async (req, res) => {
 
-  const date = new Date(2022, 01, 29, 23, 59,59);
+  const Sdate = new Date(2022, 01, 3, 08,0,0);
+  const Edate = new Date(2022, 01, 3, 12,59,59);
 
-  const isClosed = date.valueOf() - new Date().valueOf() < 0;
+  const isClosed = (Sdate.valueOf() - new Date().valueOf() > 0) || (Edate.valueOf() - new Date().valueOf() < 0) ;
+  console.log((Sdate.valueOf() - new Date().valueOf() > 0));
+  console.log((Edate.valueOf() - new Date().valueOf() < 0));
 
-  if (isClosed) return res.status(451).send("Results will be declared on "+date+", and can be viewed only for the day");
+
+  if (isClosed) return res.status(451).send("Results will be declared on "+Sdate.toLocaleString()+", and can be viewed only for the day");
 
   const q = query(collection(db, "Candidate"), orderBy("Votes", "desc"))
 
